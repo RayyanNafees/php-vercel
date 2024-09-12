8d94f6d5c17c897eb264ef91b1446d66ab804226
9node <rayyan@theninthnode.com>
2024-09-13 02:33:50 +0530

    updates

--- api/index.php
+++ api/index.php
@@ -1,42 +1,13 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>Product List</title>
-    <style>
-        .product {
-        	      border: 1px solid #ccc;
-        	            padding: 15px;
-        	                  margin-bottom: 10px;
-        	                        float: left;
-        	                              width: 30%;
-        	                                  }
+<body>
 
-        	                                      .product img {
-        	                                      	      max-width: 100%;
-        	                                      	            height: auto;
-        	                                      	                }
-        	                                      	                  </style>
-        	                                      	                  </head>
-        	                                      	                  <body>
+<?php
+header('Content-Type: text/html')
+ECHO "Hello World!<br>";
+echo "Hello World!<br>";
+EcHo "Hello World!<br>";
+?>
 
-        	                                      	                    <h1>Our Products</h1>
-
-        	                                      	                      <?php
-        	                                      	                          // 1. Include your product data
-        	                                      	                              include 'products.php';
-
-        	                                      	                                  // 2. Loop through the $products array
-        	                                      	                                      foreach ($products as $product) {
-        	                                      	                                      	  ?>
-        	                                      	                                      	        <div class="product">
-        	                                      	                                      	                <img src="<?php echo $product['image']; ?>" alt="<?php echo $product['name']; ?>">
-        	                                      	                                      	                        <h2><?php echo $product['name']; ?></h2>
-        	                                      	                                      	                                <p>$<?php echo $product['price']; ?></p>
-        	                                      	                                      	                                      </div>
-        	                                      	                                      	                                        <?php
-        	                                      	                                      	                                            } // End of the foreach loop
-        	                                      	                                      	                                              ?>
-
-        	                                      	                                      	                                              </body>
-        	                                      	                                      	                                              </html>
-  
+</body>
+</html>
