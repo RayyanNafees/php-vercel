b6716146527d0c56b041115790a46a91d9a0f4d6
9node <rayyan@theninthnode.com>
2024-09-13 02:41:31 +0530

    updates

--- api/index.php
+++ api/index.php
@@ -3,10 +3,10 @@
 <body>
 
 <?php
-header('Content-Type: text/html')
-ECHO "Hello World!<br>";
-echo "Hello World!<br>";
-EcHo "Hello World!<br>";
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
 ?>
 
 </body>
