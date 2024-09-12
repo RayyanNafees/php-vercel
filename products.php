15a424c2dfb7ab226b2f0030d4239c7a26c1d060
9node <rayyan@theninthnode.com>
2024-09-13 02:26:15 +0530

    :taga: begin: PHP testing

---
+++ products.php
@@ -0,0 +1,23 @@
+<?php
+  $products = [
+  	    [
+  	    	      'name' => 'Laptop',
+  	    	            'price' => 800,
+  	    	                  'image' => 'laptop.jpg'
+  	    	                      ],
+  	    	                          [
+  	    	                          	      'name' => 'Tablet',
+  	    	                          	            'price' => 350,
+  	    	                          	                  'image' => 'tablet.jpg'
+  	    	                          	                      ],
+  	    	                          	                          [
+  	    	                          	                          	      'name' => 'Headphones',
+  	    	                          	                          	            'price' => 150,
+  	    	                          	                          	                  'image' => 'headphones.jpg'
+  	    	                          	                          	                      ]
+  	    	                          	                          	                        ];
+  	    	                          	                          	                        ?>
+  	    	                          	                          ]
+  	    	                          ]
+  	    ]
+  ]
